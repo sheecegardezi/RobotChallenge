a4e1997a97dcea1929f96cc2db6c0536cd9b502a
Sheece Gardezi <sheecegardezi@gmail.com>
2022-03-01 15:29:34 +1100

    add setup files for pip packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='robotchallenge',
+    version='1.0.0',
+    license='Apache 2.0',
+    author="Sheece Gardezi",
+    author_email='sheecegardezi@gmail.com',
+    packages=find_packages('robotchallenge'),
+    package_dir={'': 'robotchallenge'},
+    url='https://github.com/sheecegardezi/RobotChallenge',
+    keywords='robot challenge',
+    install_requires=[
+          'pytest',
+      ],
+)
