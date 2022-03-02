9d4d2101044b14fdc4870c5e5f9084217872872d
Sheece Gardezi <sheecegardezi@gmail.com>
2022-03-03 02:22:53 +1100

    add sphinx template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -54,5 +53,3 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
