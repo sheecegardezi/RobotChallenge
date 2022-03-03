7d3f978da1d794beca66dbab8ebaeb8ffe31e473
Sheece Gardezi <sheecegardezi@gmail.com>
2022-03-03 15:10:03 +1100

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='robotchallenge',
-    version='1.0.0',
+    version='1.0.1',
     license='Apache 2.0',
     author="Sheece Gardezi",
     author_email='sheecegardezi@gmail.com',
